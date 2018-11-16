ba2438732b3ee73ea632de770ea035793e550ff0
Sam Roberts <samroberts707@gmail.com>
2018-11-16 11:06:29 +0000

    Implement attacking functionality along with win/lose conditions

--- vue-js/P1 - The Monster Slayer/app.js
+++ vue-js/P1 - The Monster Slayer/app.js
@@ -5,4 +5,41 @@ new Vue({
     monsterHealth: 100,
     gameIsRunning: false,
   },
+  methods: {
+    calcDamage: function(min,max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    startGame: function() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.gameIsRunning = true;
+    },
+    attack: function() {
+      this.monsterHealth -= this.calcDamage(3,10);
+
+      if(this.monsterHealth <= 0) {
+        alert('You Won!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      this.playerHealth -= this.calcDamage(5,12);
+
+      if(this.playerHealth <= 0) {
+        alert('You Lost!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
+    },
+  }
 });
