c1c0499023796654eb8b2534c97a78c8f00d02b5
Sam Roberts <samroberts707@gmail.com>
2018-11-16 11:41:02 +0000

    Add comments to each method of what they do // Add give up functionality

--- vue-js/P1 - The Monster Slayer/app.js
+++ vue-js/P1 - The Monster Slayer/app.js
@@ -6,11 +6,13 @@ new Vue({
     gameIsRunning: false,
   },
   methods: {
+    // Start game method to initialize variables
     startGame: function() {
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.gameIsRunning = true;
     },
+    // Basic Attack against monster
     attack: function() {
       this.monsterHealth -= this.calcDamage(3,10);
       if(this.checkWin()) {
@@ -18,6 +20,7 @@ new Vue({
       }
       this.monsterAttack();
     },
+    // Special Attack against monster with increased damage
     specialAttack: function() {
       this.monsterHealth -= this.calcDamage(10,20);
       if(this.checkWin()) {
@@ -25,6 +28,7 @@ new Vue({
       }
       this.monsterAttack();
     },
+    // Heal the player for 10, capped at 100 health points
     heal: function() {
       if(this.playerHealth <= 90) {
         this.playerHealth += 10;
@@ -33,12 +37,15 @@ new Vue({
       }
       this.monsterAttack();
     },
+    // Set game running to false to force a game reset
     giveUp: function() {
-
+      this.gameIsRunning = false;
     },
+    // Calculate the random amount of damage to be applied
     calcDamage: function(min,max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
+    // Check if the player has won or lost
     checkWin: function() {
       if(this.monsterHealth <= 0) {
         if(confirm('You won! New Game')){
@@ -57,6 +64,7 @@ new Vue({
       }
       return false;
     },
+    // Monster attacks the player
     monsterAttack: function() {
       this.playerHealth -= this.calcDamage(5,12);
       this.checkWin();
