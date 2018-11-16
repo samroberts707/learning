aabac456f55dfe4943ed331af0ab542571561b6a
Sam Roberts <samroberts707@gmail.com>
2018-11-16 11:32:05 +0000

    Add Special Attack functionality as well as some code refactoring

--- vue-js/P1 - The Monster Slayer/app.js
+++ vue-js/P1 - The Monster Slayer/app.js
@@ -6,9 +6,6 @@ new Vue({
     gameIsRunning: false,
   },
   methods: {
-    calcDamage: function(min,max) {
-      return Math.max(Math.floor(Math.random() * max) + 1, min);
-    },
     startGame: function() {
       this.playerHealth = 100;
       this.monsterHealth = 100;
@@ -16,30 +13,48 @@ new Vue({
     },
     attack: function() {
       this.monsterHealth -= this.calcDamage(3,10);
-
-      if(this.monsterHealth <= 0) {
-        alert('You Won!');
-        this.gameIsRunning = false;
+      if(this.checkWin()) {
         return;
       }
-
-      this.playerHealth -= this.calcDamage(5,12);
-
-      if(this.playerHealth <= 0) {
-        alert('You Lost!');
-        this.gameIsRunning = false;
-        return;
-      }
-
+      this.monsterAttack();
     },
     specialAttack: function() {
-
+      this.monsterHealth -= this.calcDamage(10,20);
+      if(this.checkWin()) {
+        return;
+      }
+      this.monsterAttack();
     },
     heal: function() {
 
     },
     giveUp: function() {
 
+    },
+    calcDamage: function(min,max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if(this.monsterHealth <= 0) {
+        if(confirm('You won! New Game')){
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if(confirm('You Lost! New Game')){
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    monsterAttack: function() {
+      this.playerHealth -= this.calcDamage(5,12);
+      this.checkWin();
     },
   }
 });
