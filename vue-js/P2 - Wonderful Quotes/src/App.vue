f5f61f942eefdb79eecd218cc2077c6d314e4249
Sam Roberts <samroberts707@gmail.com>
2018-11-28 17:05:24 +0000

    Add the finishing touches to the project

--- vue-js/P2 - Wonderful Quotes/src/App.vue
+++ vue-js/P2 - Wonderful Quotes/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
       <app-new-quote @quoteAdded="newQuote"></app-new-quote>
       <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
       <div class="row">
@@ -13,6 +14,7 @@
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
 
     export default {
       data: function() {
@@ -25,7 +27,11 @@
       },
       methods: {
         newQuote(quote) {
-          this.quotes.push(quote);
+          if (this.quotes.length >= this.maxQuotes) {
+            return alert('Please delete Quotes first!');
+          } else {
+            this.quotes.push(quote);
+          }
         },
         deleteQuote(index) {
           this.quotes.splice(index, 1);
@@ -33,7 +39,8 @@
       },
       components: {
         appQuoteGrid: QuoteGrid,
-        appNewQuote: NewQuote
+        appNewQuote: NewQuote,
+        appHeader: Header
       },
     }
 </script>
