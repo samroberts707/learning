d93cfe9a4486f6026651f2a9b6013c471cc986dc
Sam Roberts <samroberts707@gmail.com>
2018-11-28 16:40:10 +0000

    Add the ability to add and remove quotes

--- vue-js/P2 - Wonderful Quotes/src/App.vue
+++ vue-js/P2 - Wonderful Quotes/src/App.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="container">
-      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
 
     export default {
       data: function() {
@@ -16,8 +23,17 @@
           maxQuotes: 10
         }
       },
+      methods: {
+        newQuote(quote) {
+          this.quotes.push(quote);
+        },
+        deleteQuote(index) {
+          this.quotes.splice(index, 1);
+        }
+      },
       components: {
-        appQuoteGrid: QuoteGrid
+        appQuoteGrid: QuoteGrid,
+        appNewQuote: NewQuote
       },
     }
 </script>
