a5c86e6abd780d42e2a72180f3e65f96095cf064
Sam Roberts <samroberts707@gmail.com>
2021-05-06 15:15:26 +0100

    Start calling WP functions in theme

--- php/fictional-university/wp-content/themes/fictional-university-theme/index.php
+++ php/fictional-university/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,10 @@
-This is our amazing custom theme.
+<?php 
+
+
+
+?>
+<!-- Get Site Title from WP Admin -->
+<h1><?php blogInfo('name'); ?></h1>
+
+<!-- Get Site Tagline from WP Admin -->
+<p><?php blogInfo('description'); ?></p>
