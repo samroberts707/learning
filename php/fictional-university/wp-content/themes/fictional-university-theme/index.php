0d49761c6e4a4dfffa9134ce964224b20ca312fb
Sam Roberts <samroberts707@gmail.com>
2021-05-06 15:27:33 +0100

    get blog posts and loop through them

--- php/fictional-university/wp-content/themes/fictional-university-theme/index.php
+++ php/fictional-university/wp-content/themes/fictional-university-theme/index.php
@@ -1,6 +1,19 @@
 <?php 
 
-
+    // Get blog posts
+    // Loop through all blog posts
+    while(have_posts()) {
+        // Get the current post in the while loop
+        the_post();?>
+        <!-- Get the link for the current post in the loop -- This includes the whole link including domain -->
+        <a href="<?php the_permalink(); ?>">
+            <!-- Get the title from the current post in the loop -->
+            <h2><?php the_title(); ?></h2>
+        </a>
+        <!-- Get the content from the current post in the loop -->
+        <p><?php the_content(); ?></p>
+        <hr />
+    <?php }
 
 ?>
 <!-- Get Site Title from WP Admin -->
